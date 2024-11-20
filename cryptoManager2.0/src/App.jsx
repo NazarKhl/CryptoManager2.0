287e442e914f08aeb4cdff1d2e4c7dad2403faa5
Nazar Khlivnyi <135509585+NazarKhl@users.noreply.github.com>
2024-11-20 15:24:51 +0100

    Add 3 more display pages

--- cryptoManager2.0/src/App.jsx
+++ cryptoManager2.0/src/App.jsx
@@ -1,17 +1,40 @@
+import { Spin } from "antd";
 import { BellOutlined } from "@ant-design/icons";
- 
+import { useState } from "react";
+
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 100);
+
   return (
     <>
+      {loading && (
+        <div className="loadingPanel">
+          <Spin size="large" />
+        </div>
+      )}
       <div className="navBar">
-        <p className="navBarLinks dashboardPanel">Dashboard</p>
-        <p className="navBarLinks portfolioPanel">Portfolio</p>
-        <p className="navBarLinks pricesPanel">Prices</p>
-        <p className="navBarLinks helpPanel">Help Suport</p>
+        <a className="navBarLinks dashboardPanel" href="#main">
+          Dashboard
+        </a>
+        <a className="navBarLinks pricesPanel" href="#prices">
+          Prices
+        </a>
+        <a className="navBarLinks helpPanel" href="#help">
+          Help Suport
+        </a>
         <div className="bellBorder">
           <BellOutlined className="bellIcon" />
         </div>
-        <p className="navBarLinks accountSettings">Example</p>
+        <p className="navBarLinks accountSettings">Portfolio</p>
+      </div>
+      <div id="container">
+        <div id="main"></div>
+        <div id="prices"></div>
+        <div id="help"></div>
       </div>
     </>
   );
